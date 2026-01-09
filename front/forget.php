155054bd3cb307ded02b0f5d4b87163218e381a0
Hall <oncemeet0857@gmail.com>
2026-01-09 11:45:16 +0800

    feat: 製作前端忘記密碼頁面

---
+++ front/forget.php
@@ -0,0 +1,13 @@
+    <fieldset>
+        <legend>忘記密碼</legend>
+        <form action="./api/forget.php" method="post">
+            <div>請輸入信箱以查詢密碼</div>
+            <div>
+                <input type="email" name="" id="">
+            </div>
+            <div id="result"></div>
+            <div>
+                <input type="submit" value="尋找">
+            </div>
+        </form>
+    </fieldset>
